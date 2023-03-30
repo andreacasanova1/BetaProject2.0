310555fb890be60e4481c6e46d176b86a6fdeb2b
andreacasanova1 <116755988+andreacasanova1@users.noreply.github.com>
2023-03-30 20:33:12 +0200

    menu burger ok

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.querySelector(".menu-burger").addEventListener("click" , animateBurger)
 let line1__burger = document.querySelector(".line1__menu-burger");
 let line2__burger = document.querySelector(".line2__menu-burger");
 let line3__burger = document.querySelector(".line3__menu-burger");
-let topbar = document.querySelector(".topbar");
+let topbar = document.querySelector(".topbar__nav-container");
 //comandos rápidos, si dejo presionado (alt) y marco los puntos que quiero
 //modificar se cambian simultaneamente.
 
@@ -14,7 +14,7 @@ function animateBurger() {
     line1__burger.classList.toggle("activeline1__menu-burger");
     line2__burger.classList.toggle("activeline2__menu-burger");
     line3__burger.classList.toggle("activeline3__menu-burger");
-    topbar.classList.toggle("topbar__menu");
+    topbar.classList.toggle("topbar__nav-container--open");
     
 }
 
